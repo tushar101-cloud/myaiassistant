90427f95619e7c223f1b9052f336aead95c436e4
tushar101-cloud <et21bthcs025@kazirangauniversity.in>
2025-11-09 19:54:28 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -30,4 +30,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
